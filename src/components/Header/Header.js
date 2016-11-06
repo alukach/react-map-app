ef93f079e53ffa5ab0d3d520f78668cc93db6c64
Anthony Lukach <anthonylukach@gmail.com>
2016-11-05 23:46:03 -0600

    Nabber active style working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
-import './Header.scss'
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
 import FlatButton from 'material-ui/FlatButton';
+import './Header.scss'
 
 export const Header = () => (
   <Toolbar>
     <ToolbarTitle text="El See Em" />
     <ToolbarGroup>
-      <IndexLink to='/' activeClassName='route--active'>
+      <IndexLink to='/' activeClassName="activeRoute">
         <FlatButton label="Home" />
       </IndexLink>
-      <Link to='/counter' activeClassName='route--active'>
+      <Link to='/counter' activeClassName="activeRoute">
         <FlatButton label="Counter" />
       </Link>
-      <Link to='/map' activeClassName='route--active'>
+      <Link to='/map' activeClassName="activeRoute">
         <FlatButton label="Map" />
       </Link>
     </ToolbarGroup>
